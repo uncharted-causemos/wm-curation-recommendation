a6c8d8abd2fa380fa6ab736df59f2f667884239d
Azad Memon <amemon@uncharted.software>
2020-08-07 16:35:55 -0400

    Modified app.py to be callable for tests

--- app.py
+++ app.py
@@ -8,8 +8,13 @@ from api.health_check import health_check_api
 from api.regrounding_recommendations import regrounding_recommendations_api
 from api.polarity_recommendations import polarity_recommendations_api
 
-app = Flask(__name__)
 
-app.register_blueprint(health_check_api, url_prefix='/health_check')
-app.register_blueprint(regrounding_recommendations_api, url_prefix='/recommendations/regrounding')
-app.register_blueprint(polarity_recommendations_api, url_prefix='/recommendations/polarity')
+def create_app(testing=False):
+    app = Flask("__name__", )
+    app.config['TESTING'] = True
+
+    app.register_blueprint(health_check_api, url_prefix='/health_check')
+    app.register_blueprint(regrounding_recommendations_api, url_prefix='/recommendations/regrounding')
+    app.register_blueprint(polarity_recommendations_api, url_prefix='/recommendations/polarity')
+
+    return app
