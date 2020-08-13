62775034730f1e5c91b5699f505943ec315a9294
Azad Memon <amemon@uncharted.software>
2020-08-13 12:36:55 -0400

    When data size is less than n_components, we use random initialization instead of spectral intialization

--- services/dimension_reduction_service.py
+++ services/dimension_reduction_service.py
@@ -2,7 +2,15 @@ from umap import UMAP
 
 
 def fit(data, n_components, min_dist):
-    return UMAP(n_components=n_components, min_dist=min_dist).fit(data)
+    # Note, there is a hard limit that the data must have more than 2 points, otherwise UMAP will fail.
+
+    # UMAP has a limitation on using spectral initialization only when n_components < data.shape
+    # See this issue: https://github.com/lmcinnes/umap/issues/201
+    if (n_components >= data.shape[0] - 1):
+        init = 'random'
+    else:
+        init = 'spectral'
+    return UMAP(n_components=n_components, min_dist=min_dist, init=init).fit(data)
 
 
 def transform(mapper, data):
