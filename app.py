e2bdb57bfdb8b643a7b2919078c1ef62bbcdfa28
Azad Memon <amemon@uncharted.software>
2020-08-27 10:12:27 -0400

    Added some basic request information logging

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from dotenv import load_dotenv, find_dotenv
+import copy
 
 # Load env before loading app specific files
 load_dotenv(find_dotenv(), override=True)
@@ -13,6 +14,15 @@ from api.errors.error_handling import errors
 def create_app(testing=False):
     app = Flask("__name__", )
     app.config['TESTING'] = True
+
+    @app.after_request
+    def after_request(response):
+        """ Logging after every request. """
+        body = copy.deepcopy(request.get_json())
+        del body['statement_ids']
+        print(f'Request body: {body}')
+
+        return response
 
     app.register_blueprint(health_check_api, url_prefix='/health_check')
     app.register_blueprint(regrounding_recommendations_api, url_prefix='/recommendations/regrounding')
