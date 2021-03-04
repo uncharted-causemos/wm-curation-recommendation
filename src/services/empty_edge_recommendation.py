180cbb020073ae3ab9c1d4987b38c6e8704a3885
Azad Memon <amemon@uncharted.software>
2021-03-04 14:24:24 -0500

    Added a description of how empty-edge suggestions work

--- src/services/empty_edge_recommendation.py
+++ src/services/empty_edge_recommendation.py
@@ -5,6 +5,26 @@ try:
 except ImportError as e:
     print(e)
 
+'''
+This finds statements who have as candidates, the subj_concept and obj_concept in question.
+
+Examples:
+a) subj.candidate.score = 0.9 and obj.candidate.score = 0.1
+b) subj.candidate.score = 0.4 and obj.candidate.score = 0.4
+c) subj.candidate.score = 0.35 and obj.candidate.score = 0.9
+
+There are three options for scoring edges:
+1. average score: Take the average of subj.candidate.score and obj.candidate.score. But this ranks example (a) above example (b).
+We want some sort of minimum standard for the candidates.
+
+2. min score: Sort edges in descending order of min(subj.candidate.score, obj.candidate.score). This would rate example (b) above (a). 
+But it would also rank it higher than example (c), whose minimum score is only slightly lower, but it's other score is much higher.
+
+3. variable threshold: Here the goal is to find a variable threshold to serve as our minimum standard for candidates. Then rate the remaining 
+edges based on their average score. The threshold is calculated as the mean(scores) - std(scores) i.e. slightly below the average of the scores of candidates
+found for the subj_concept and obj_concept in question. 
+'''
+
 
 def get_edge_recommendations(project_id, subj_concept, obj_concept):
     body = {
